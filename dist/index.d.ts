4957bd276ad0ecc5341db6e5601b1b8a6406eeaa
ez1n <eodrmfdl1004@naver.com>
2024-04-13 01:12:49 +0900

    fix: dist 업로드

---
+++ dist/index.d.ts
@@ -0,0 +1,1 @@
+export declare function printName(name: string): string;
---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.printName = void 0;
+function printName(name) {
+    return name;
+}
+exports.printName = printName;
